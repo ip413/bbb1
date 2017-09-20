331e667bd5726613769922261e2181dc7f659919
Andrzej Salak <andrzej.salak@gmail.com>
2017-09-20 13:29:06 +0200

    sign up not working

--- src/NavbarUI.js
+++ src/NavbarUI.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Tab from 'material-ui/Tabs/Tab';
 import Tabs from 'material-ui/Tabs/Tabs';
+import SignUp from './SignUp';
 
 class NavbarUi extends Component {
   render() {
     return (
       <div>
         <Tabs>
-          <Tab label="Sign up"></Tab>
+          <Tab label="Sign up"><SignUp /></Tab>
           <Tab label="Log in"></Tab>
           <Tab label="Change Password"></Tab>
           <Tab label="Log out" className="danger-background"></Tab>
